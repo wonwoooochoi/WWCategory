7f38f4b6fc4d4d45eb6169953aab3b5f522a6629
Won Woo Choi <wonwoooochoi@gmail.com>
2016-01-10 18:43:31 +0900

    Add HMAC category to NSString.

---
+++ Category/Foundation/NSString/NSString+HMAC.h
@@ -0,0 +1,20 @@
+//
+//  NSString+HMAC.h
+//  WWCategory
+//
+//  Created by ww on 2016. 1. 10..
+//  Copyright © 2016년 Won Woo Choi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (HMAC)
+
+/**
+ @brief HMAC SHA256 해시 문자열을 생성.
+ @param key 문자열 키.
+ @return 문자열.
+ */
+- (NSString *)SHA256WithKey:(NSString *)key;
+
+@end
---
+++ Category/Foundation/NSString/NSString+HMAC.m
@@ -0,0 +1,35 @@
+//
+//  NSString+HMAC.m
+//  WWCategory
+//
+//  Created by ww on 2016. 1. 10..
+//  Copyright © 2016년 Won Woo Choi. All rights reserved.
+//
+
+#import "NSString+HMAC.h"
+#import <CommonCrypto/CommonHMAC.h>
+
+@implementation NSString (HMAC)
+
+- (NSString *)SHA256WithKey:(NSString *)key {
+	
+	const char *cKey = [key cStringUsingEncoding:NSASCIIStringEncoding];
+	const char *cData = [self cStringUsingEncoding:NSASCIIStringEncoding];
+	
+	unsigned char cHMAC[CC_SHA256_DIGEST_LENGTH];
+	
+	CCHmac(kCCHmacAlgSHA256, cKey, strlen(cKey), cData, strlen(cData), cHMAC);
+	
+	NSData *HMAC = [[NSData alloc] initWithBytes:cHMAC
+													  length:sizeof(cHMAC)];
+	
+	NSString *hash = HMAC.description;
+	hash = [hash stringByReplacingOccurrencesOfString:@" " withString:@""];
+	hash = [hash stringByReplacingOccurrencesOfString:@"<" withString:@""];
+	hash = [hash stringByReplacingOccurrencesOfString:@">" withString:@""];
+	
+	return hash;
+	
+}
+
+@end
