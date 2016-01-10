5dafe6b56eb32a5721d0e27ee27afe8486f9fd6c
Won Woo Choi <wonwoooochoi@gmail.com>
2016-01-10 19:42:48 +0900

    Add Rounding category to UIView.

---
+++ Category/UIKit/UIView/UIView+Rounding.h
@@ -0,0 +1,20 @@
+//
+//  UIView+Rounding.h
+//  WWCategory
+//
+//  Created by ww on 2016. 1. 10..
+//  Copyright © 2016년 Won Woo Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Rounding)
+
+/**
+ @brief 뷰의 가장자리 코너 4군데중 선택한 코너를 라운딩 처리.
+ @param roundingCorners 라운딩할 코너들.
+ @param cornerRadius 라운딩 값.
+ */
+- (void)setRoundingCorners:(UIRectCorner)roundingCorners cornerRadius:(CGFloat)cornerRadius;
+
+@end
---
+++ Category/UIKit/UIView/UIView+Rounding.m
@@ -0,0 +1,39 @@
+//
+//  UIView+Rounding.m
+//  WWCategory
+//
+//  Created by ww on 2016. 1. 10..
+//  Copyright © 2016년 Won Woo Choi. All rights reserved.
+//
+
+#import "UIView+Rounding.h"
+
+@implementation UIView (Rounding)
+
+- (void)setRoundingCorners:(UIRectCorner)roundingCorners cornerRadius:(CGFloat)cornerRadius {
+	
+	UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:self.bounds
+																  byRoundingCorners:roundingCorners
+																		  cornerRadii:CGSizeMake(cornerRadius, cornerRadius)];
+	
+	CAShapeLayer *maskLayer = nil;
+	
+	if ([self.layer.mask isKindOfClass:[CAShapeLayer class]]) {
+		
+		maskLayer = (CAShapeLayer *)self.layer.mask;
+		
+	}
+	else {
+		
+		maskLayer = [CAShapeLayer layer];
+		
+	}
+	
+	[maskLayer setFrame:self.bounds];
+	[maskLayer setPath:maskPath.CGPath];
+	
+	[self.layer setMask:maskLayer];
+	
+}
+
+@end
