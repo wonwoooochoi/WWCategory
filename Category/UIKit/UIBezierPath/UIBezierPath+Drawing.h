7eb1002e08abbd53b3cfa1c2f8cc11d1216fb164
Won Woo Choi <wonwoooochoi@gmail.com>
2016-01-10 19:35:38 +0900

    Add drawing category to UIBezierPath.

---
+++ Category/UIKit/UIBezierPath/UIBezierPath+Drawing.h
@@ -0,0 +1,23 @@
+//
+//  UIBezierPath+Drawing.h
+//  WWCategory
+//
+//  Created by ww on 2016. 1. 10..
+//  Copyright © 2016년 Won Woo Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIBezierPath (Drawing)
+
+/**
+ @brief 호를 그림.
+ @param center 중심정 좌표.
+ @param radius 반지름.
+ @param startDegree 시작 각.
+ @param endDegree 종료 각.
+ @param clockwise 시계방향 여부.
+ */
+- (void)addArcWithCenter:(CGPoint)center radius:(CGFloat)radius startDegree:(CGFloat)startDegree endDegree:(CGFloat)endDegree clockwise:(BOOL)clockwise;
+
+@end
---
+++ Category/UIKit/UIBezierPath/UIBezierPath+Drawing.m
@@ -0,0 +1,19 @@
+//
+//  UIBezierPath+Drawing.m
+//  WWCategory
+//
+//  Created by ww on 2016. 1. 10..
+//  Copyright © 2016년 Won Woo Choi. All rights reserved.
+//
+
+#import "UIBezierPath+Drawing.h"
+
+@implementation UIBezierPath (Drawing)
+
+- (void)addArcWithCenter:(CGPoint)center radius:(CGFloat)radius startDegree:(CGFloat)startDegree endDegree:(CGFloat)endDegree clockwise:(BOOL)clockwise {
+	
+	[self addArcWithCenter:center radius:radius startAngle:((startDegree - 90) / 180 * M_PI) endAngle:((endDegree - 90) / 180 * M_PI) clockwise:clockwise];
+	
+}
+
+@end
