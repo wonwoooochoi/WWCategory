820844082e32b2d867fc2a11c72c6f00fb2e23f1
Wonwoo Choi <wonwoo@cyworld.biz>
2018-06-04 12:25:17 +0900

    Fix comments.

--- Category/UIKit/UIImage/UIImage+CreateImage.h
+++ Category/UIKit/UIImage/UIImage+CreateImage.h
@@ -13,12 +13,14 @@
 
 /**
  지정한 경로에 있는 이미지 파일의 이미지 객체 생성.
+ @param url 이미지 경로 URL 객체.
  @return 이미지 객체.
  */
 + (UIImage *)imageWithURL:(NSURL *)url;
 
 /**
  PNG파일의 파일명으로 이미지 객체 생성.
+ @param imageName PNG 이미지 파일명.
  @return 이미지 객체.
  */
 + (UIImage *)imageWithPNGName:(NSString *)imageName;
@@ -26,6 +28,8 @@
 
 /**
  PNG파일의 파일명으로 늘어날 이미지 객체 생성.
+ @param imageName PNG 이미지 파일명.
+ @param inset 늘어날 영역.
  @return 이미지 객체.
  */
 + (UIImage *)imageWithPNGName:(NSString *)imageName capInsets:(UIEdgeInsets)inset;
@@ -33,6 +37,8 @@
 
 /**
  지정한 컬러와 사이즈의 이미지 객체 생성.
+ @param color 컬러 객체.
+ @param size 이미지 사이즈.
  @return 이미지 객체.
  */
 + (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size;
